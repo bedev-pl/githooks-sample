ca81501a1554254ac641939b5eaecffe334bb1bb
BeDev.pl <kontakt@bedev.pl>
2019-08-22 23:30:05 +0200

    RBMK-1003: user add impl

---
+++ src/Controller/UserController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController
+{
+    public function addUser(): Response
+    {
+        return new Response();
+    }
+}
